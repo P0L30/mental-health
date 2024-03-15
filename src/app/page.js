c537fb3e2028fa555861020ed028694c8f1918d8
Энхмөнх Найдандорж <HP230373@G303-CC0146.local>
2024-03-15 19:17:32 +0800

    command-second

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,14 @@
 'use client'
 
 import Image from "next/image";
-import { useState, useMemo } from "react"
+import { useState, useEffect } from "react"
 import { useRouter } from "next/navigation";
 export default function Postsections() {
-  const myData = [
-    "More...",
-    "Deppression",
-    "Anxiety disorder",
-    "Schizophrenia",
-    "Eating disorder",
-    "PTSD",
-    "Autism",
-    "ADHD",
-    "Insomnia",
-    "Bipolar Disorder"
-  ]
+  const myData = ["More...", "Deppression", "Anxiety disorder", "Schizophrenia", "Eating disorder", "PTSD", "Autism", "ADHD", "Insomnia", "Bipolar Disorder"]
   const router = useRouter()
   const [com, setCom] = useState("")
   const [post, setPost] = useState({})
   const [postArray, setPostArray] = useState([])
-  const [herData, setHerData] = useState([])
   function handleTitle(e) {
     setPost((prev) => ({ ...prev, title: e.target.value }))
   }
@@ -28,15 +16,17 @@ export default function Postsections() {
   function handleDescription(e) {
     setPost((prev) => ({ ...prev, description: e.target.value }))
   }
+  function returnComment(e) {
+    setCom(e.target.value)
+  }
 
   function handleDelete() {
     if (post.title == "" && post.description == "") {
-
+      alert("Invalid title and description")
     } else {
       if (confirm("Are you sure about deleting?")) {
-        setPost(post.title(""))
+        setPost(post.title())
       }
-
     }
   }
 
@@ -44,76 +34,61 @@ export default function Postsections() {
     if (post.title == "" || post.description == "") {
       alert("Invalid title or description")
     } else {
-      setPostArray(prev => ([...prev, { ...post, comments: [], tags: [] }]))
+      setPostArray(prev => ([...prev, { ...post, comments: [],}]))
       alert("success")
       console.log(postArray)
 
     }
   }
 
-  function seeClick(option) {
-
-  }
-
-
-
   function handleComment(index) {
     alert("success")
     postArray[index].comments.push(com)
     setCom("")
     console.log(postArray)
-
-  }
-  function returnComment(e) {
-    setCom(e.target.value)
   }
 
-  function searchNavbar(e) {
-    myData.filter((prev) => prev)
+  function seeType(e) {
+    console.log(e.target.value)
+    setPost((prev) => ({ ...prev, tags: [...(prev.tags ?? []), e.target.value] }))
+    console.log(post.tags)
   }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="post-section">
         <div className="user-section">
           <img src="" />
-          <button onClick={() => { router.push('https://www.youtube.com/watch?v=eauWX_UQ8HE') }}>CLick me!</button>
+          <button onClick={() => { router.push('https://www.youtube.com/watch?v=LzkTbS2PXNM&rco=1') }}>Click me!</button>
           <div className="username-section">
-            <p className="username">John Walker</p>
-            <div className="tag-title">
+            <p className="username" style={{ marginTop: 10 }}>John Walker</p>
+            <div style={{ display: "flex", flexDirection: "row", gap: 20, marginTop: 10 }}>
               <p>Tags:</p>
-
-              <select style={{ color: "black" }} onChange={(_, index) => addList(index)}>
-                {myData.map((text, index) => {
+              <select style={{ color: "black" }} onChange={seeType} >
+                {myData.map((text) => {
                   return (
-                    <option >{text}</option>
+                    <option>{text}</option>
                   )
                 })}
               </select>
-
+              <p className="time">17 hours ago</p>
 
             </div>
           </div>
-          <p className="time">17 hours ago</p>
+
         </div>
-        <div className="desc-section" style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-          <select>
-            {myData.map((test, index) => {
-              return (
-                <option className="option-select">a</option>
-              )
-            })}
-          </select>
-          <input onChange={handleTitle} className="title" placeholder="Write your title here!" value={post.title} style={{ color: "black" }} />
-          <textarea onChange={handleDescription} className="desc" placeholder="Write your description here!" value={post.description} style={{ color: " black", }} />
+        <div className="desc-section" style={{ display: "flex", flexDirection: "row", gap: "20px", marginTop: 10 }}>
+          <input onChange={handleTitle} className="title" placeholder="Write your title here!" value={post.title} style={{ color: "black", height: 30, width: 250 }} />
+          <input onChange={handleDescription} className="desc" placeholder="Write your description here!" value={post.description} style={{ color: " black", width: 250 }} />
         </div>
 
-        <div className="button-section">
+        <div style={{ marginTop: 10 }} className="button-section">
           <button className="btn" onClick={handleDelete}>Delete Post</button>
           <button className="btn" onClick={handlePost} style={{ marginLeft: 20 }}>Post</button>
 
         </div>
 
-        <div className="post-map" style={{ border: 1, border: "white", border: "solid", padding: 10, display: "flex", flexDirection: "column", gap: 10 }}>
+        <div className="post-map" style={{ border: 1, border: "white", border: "solid", padding: 10, display: "flex", flexDirection: "column", gap: 10, marginTop: 50 }}>
           {postArray.map((text, index) => {
 
             return (
@@ -123,13 +98,11 @@ export default function Postsections() {
                   <div className="username-section">
                     <p className="username">John Walker</p>
                     <p className="tag-title">Tags:</p>
-                    <select style={{ color: "black" }} onChange={(_, option) => seeClick()}>
-                      {myData.map((text) => {
-                        return (
-                          <option >{text}</option>
-                        )
-                      })}
-                    </select>
+                    {postArray.map((text) => {
+                      return (
+                        <p style={{ color: "black", background: "white", paddingRight: 20 }}>{text.tags}</p>
+                      )
+                    })}
                   </div>
                   <p className="time">17 hours ago</p>
                 </div>
@@ -141,7 +114,6 @@ export default function Postsections() {
                   <button className="btn">Like</button>
                   <button className="btn">Comment</button>
                   <button className="btn">Share</button>
-                  <button className="btn">Save</button>
                 </div>
                 <div style={{ display: "flex", flexDirection: "row", gap: 20 }}>
                   <img />
@@ -164,54 +136,7 @@ export default function Postsections() {
           })}
         </div>
 
-        <input className="search-navbar" placeholder="Search here!" onChange={() => searchNavbar()} />
-
-        <div>
-
-          {herData.map((text) => {
-            return (
-              <button>{text}</button>
-            )
-          })}
-          <button style={{ width: 100, height: 100, background: "white", color: "black" }}>PTSD</button>
-          <button style={{ width: 100, height: 100, background: "white", color: "black" }}>Deppression</button>
-          <button style={{ width: 100, height: 100, background: "white", color: "black" }}>Anxiety</button>
-          <select style={{ color: "black" }}>
-            {myData.map((text, index) => {
-              return (
-                <option onClick={() => addList(index)}>{text}</option>
-              )
-            })}
-          </select>
-        </div>
       </div>
-
-
     </main>
   );
 }
-export function Post() {
-  return (
-    <main style={{ background: "white", width: "100vw", height: "100vh", display: "flex", flexDirection: "column"}}> 
-      <div>
-        <p></p>
-        <div>
-          <p></p>
-          <select>
-            <option></option>
-          </select>
-          <p></p>
-        </div>
-          <div></div>
-          <div></div>
-          <div>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
-          </div>
-      </div>
-    </main>
-  )
-}
-
